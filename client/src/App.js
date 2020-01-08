b57a8381e97c1146eafa007817ab41e558ef1568
Avi Konduru <avikonduru@gmail.com>
2020-01-07 20:52:00 -0500

    Setted up react and client side of app

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+const App = () => {
+	return (
+		<Fragment>
+			<h1>App</h1>
+		</Fragment>
+	);
+};
+
+export default App;

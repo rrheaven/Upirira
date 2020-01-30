5b30aac3f49564ebb0cbc6b117530a334c481a8b
Avi Konduru <avikonduru@gmail.com>
2020-01-29 22:38:17 -0500

    production version 1

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -18,6 +19,16 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/transactions', require('./routes/api/transactions'));
 app.use('/api/receivers', require('./routes/api/receivers'));
 app.use('/api/plaid', require('./routes/api/plaid'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 
